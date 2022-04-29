59e06d1e7abca7be34f499d275e165a1970efaa6
Guilhermefn <103532923+Guilhermefn@users.noreply.github.com>
2022-04-29 08:12:53 -0300

    commit

--- Java/Aula_9/Herança/Pessoa.java
+++ Java/Aula_9/Herança/Pessoa.java
@@ -6,4 +6,15 @@ public class Pessoa {
     public Pessoa (String nome) {
         this.nome = nome;
     }
+    public Pessoa (int idade, String nome) {
+        this.idade = idade;
+        this.nome = nome;
+    }
+    // modificadores
+    public void setIdade (int idade) {
+        this.idade = idade;
+    }
+    public void setNome (String nome) {
+        this.nome = nome;
+    }
 }
